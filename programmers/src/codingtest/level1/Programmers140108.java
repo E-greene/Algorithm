214a995d5e3dbd828d64ab618461cc708bc30339
E-greene <phoex1106@gmail.com>
2023-01-27 17:18:28 +0900

    문자열 나누기

--- programmers/src/codingtest/level1/Programmers140108.java
+++ programmers/src/codingtest/level1/Programmers140108.java
@@ -6,10 +6,41 @@ public class Programmers140108 {
 
     public static void main (String args[]) {
         Programmers140108 programmers140108 = new Programmers140108();
-        int result = programmers140108.solutionFail("banana");
+        int result = programmers140108.solution("aaabbaccccabba");
         System.out.println(result);
     }
-    
+
+    public int solution(String s) {
+        int answer = 0;
+
+        char c = s.charAt(0);
+        int countEqual = 0;
+        int countNonEqual =0;
+
+        for(int i = 0; i< s.length(); i++) {
+
+            if(s.charAt(i) == c) {
+                countEqual++;
+            }else {
+                countNonEqual++;
+            }
+
+            if(countEqual == countNonEqual) {
+                answer++;
+                if(i+1 == s.length()) {
+                    break;
+                }else {
+                    c = s.charAt(i+1);
+                }
+            }
+        }
+        if(countEqual>countNonEqual) {
+            answer++;
+        }
+
+        return answer;
+    }
+
     //시간초과 뜸
     public int solutionFail(String s) {
         int answer = 0;
